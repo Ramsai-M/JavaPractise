f0f59513b50282a42407cf132bc69f4a013c78ef
Ramsai <85005737+Ramsai-M@users.noreply.github.com>
2023-09-17 09:27:06 +0530

    Print numbers from 1 to N

---
+++ Nnum1toN.java
@@ -0,0 +1,13 @@
+//Print numbers from 1 to N
+
+import java.util.Scanner;
+
+public class Nnum1toN{
+public static void main(String[] args){
+Scanner reader=new Scanner(System.in);
+int n=1;
+for(int i=n;i<=10;i++){
+System.out.println(i);
+}
+}
+}
